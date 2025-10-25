1f49d6330c8051c5256c5f823820f989ef0f37d3
alkymvaletta <alkymvaletta@gmail.com>
2025-10-24 21:50:28 -0500

    feat: Desarrollo clase Profesor

---
+++ EjercicioJAVA/src/Profesor.java
@@ -0,0 +1,29 @@
+public class Profesor {
+    protected String nombre;
+    protected String programa;
+
+    public Profesor (String nombre, String programa){
+        this.nombre = nombre;
+        this.programa = programa;
+        }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+    
+        
+
+    
+}
